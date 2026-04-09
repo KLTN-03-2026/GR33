5fb8d1948c1a50f540deb4ea28056eca7dd413ac
caothanhbinh204 <caothanhbinh246@gmail.com>
2026-04-10 00:11:10 +0700

    Cập nhật master page

---
+++ frontend/src/layout/wrapper/Admin/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-shell">
+    <Sidebar :sidebar-collapsed="collapsed" />
+    <Navbar 
+      :sidebar-collapsed="collapsed" 
+      @toggle-sidebar="collapsed = !collapsed"
+    />
+    <main class="main-content" :class="{ 'sidebar-collapsed': collapsed }">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import Sidebar from '../../components/Admin/Sidebar.vue'
+import Navbar from '../../components/Admin/Navbar.vue'
+
+export default {
+  name: 'AdminLayout',
+  components: { Sidebar, Navbar },
+  data() {
+    return { 
+      collapsed: false
+    }
+  }
+}
+</script>
