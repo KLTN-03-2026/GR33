c7902df4996b4ae130101aaf26a46e5846b7b306
Phong Hoàng (Pong) <vanphong92703@gmail.com>
2026-05-02 11:39:58 +0700

    Sửa file Deploy.sh trong SC

--- smart_contracts/deploy.sh
+++ smart_contracts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /Users/pong/Documents/Code/Project/KHOALUANTOTNGHIEP/smart_contracts
+cd /Users/pong/Documents/Code/Project/GR33/smart_contracts
 
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 OWNER_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
